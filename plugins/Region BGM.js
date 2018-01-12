1099bb0eb13001c7054a20faa4d8bf2f0859cc0e
DaedraKyne <34008496+DaedraKyne@users.noreply.github.com>
2018-01-12 23:16:44 +0100

    Create Region BGM.js

---
+++ plugins/Region BGM.js
@@ -0,0 +1,893 @@
+//-----------------------------------------------------------------------------
+// DaedraKyne Plugins - Region BGM - BGM sounds for each region!
+//                          -------
+//                       Region BGM.js
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+ /*:
+ * @plugindesc v1.0
+ * 
+ * @author DaedraKyne
+ * 
+ * @param region 1
+ * @type struct<Region>
+ * 
+ * @param region 2
+ * @type struct<Region>
+ * 
+ * @param region 3
+ * @type struct<Region>
+ * 
+ * @param region 4
+ * @type struct<Region>
+ * 
+ * @param region 5
+ * @type struct<Region>
+ * 
+ * @param region 6
+ * @type struct<Region>
+ * 
+ * @param region 7
+ * @type struct<Region>
+ * 
+ * @param region 8
+ * @type struct<Region>
+ * 
+ * @param region 9
+ * @type struct<Region>
+ * 
+ * @param region 10
+ * @type struct<Region>
+ * 
+ * @param region 11
+ * @type struct<Region>
+ * 
+ * @param region 12
+ * @type struct<Region>
+ * 
+ * @param region 13
+ * @type struct<Region>
+ * 
+ * @param region 14
+ * @type struct<Region>
+ * 
+ * @param region 15
+ * @type struct<Region>
+ * 
+ * @param region 16
+ * @type struct<Region>
+ * 
+ * @param region 17
+ * @type struct<Region>
+ * 
+ * @param region 18
+ * @type struct<Region>
+ * 
+ * @param region 19
+ * @type struct<Region>
+ * 
+ * @param region 20
+ * @type struct<Region>
+ * 
+ * @param region 21
+ * @type struct<Region>
+ * 
+ * @param region 22
+ * @type struct<Region>
+ * 
+ * @param region 23
+ * @type struct<Region>
+ * 
+ * @param region 24
+ * @type struct<Region>
+ * 
+ * @param region 25
+ * @type struct<Region>
+ * 
+ * @param region 26
+ * @type struct<Region>
+ * 
+ * @param region 27
+ * @type struct<Region>
+ * 
+ * @param region 28
+ * @type struct<Region>
+ * 
+ * @param region 29
+ * @type struct<Region>
+ * 
+ * @param region 30
+ * @type struct<Region>
+ * 
+ * @param region 31
+ * @type struct<Region>
+ * 
+ * @param region 32
+ * @type struct<Region>
+ * 
+ * @param region 33
+ * @type struct<Region>
+ * 
+ * @param region 34
+ * @type struct<Region>
+ * 
+ * @param region 35
+ * @type struct<Region>
+ * 
+ * @param region 36
+ * @type struct<Region>
+ * 
+ * @param region 37
+ * @type struct<Region>
+ * 
+ * @param region 38
+ * @type struct<Region>
+ * 
+ * @param region 39
+ * @type struct<Region>
+ * 
+ * @param region 40
+ * @type struct<Region>
+ * 
+ * @param region 41
+ * @type struct<Region>
+ * 
+ * @param region 42
+ * @type struct<Region>
+ * 
+ * @param region 43
+ * @type struct<Region>
+ * 
+ * @param region 44
+ * @type struct<Region>
+ * 
+ * @param region 45
+ * @type struct<Region>
+ * 
+ * @param region 46
+ * @type struct<Region>
+ * 
+ * @param region 47
+ * @type struct<Region>
+ * 
+ * @param region 48
+ * @type struct<Region>
+ * 
+ * @param region 49
+ * @type struct<Region>
+ * 
+ * @param region 50
+ * @type struct<Region>
+ * 
+ * @param region 51
+ * @type struct<Region>
+ * 
+ * @param region 52
+ * @type struct<Region>
+ * 
+ * @param region 53
+ * @type struct<Region>
+ * 
+ * @param region 54
+ * @type struct<Region>
+ * 
+ * @param region 55
+ * @type struct<Region>
+ * 
+ * @param region 56
+ * @type struct<Region>
+ * 
+ * @param region 57
+ * @type struct<Region>
+ * 
+ * @param region 58
+ * @type struct<Region>
+ * 
+ * @param region 59
+ * @type struct<Region>
+ * 
+ * @param region 60
+ * @type struct<Region>
+ * 
+ * @param region 61
+ * @type struct<Region>
+ * 
+ * @param region 62
+ * @type struct<Region>
+ * 
+ * @param region 63
+ * @type struct<Region>
+ * 
+ * @param region 64
+ * @type struct<Region>
+ * 
+ * @param region 65
+ * @type struct<Region>
+ * 
+ * @param region 66
+ * @type struct<Region>
+ * 
+ * @param region 67
+ * @type struct<Region>
+ * 
+ * @param region 68
+ * @type struct<Region>
+ * 
+ * @param region 69
+ * @type struct<Region>
+ * 
+ * @param region 70
+ * @type struct<Region>
+ * 
+ * @param region 71
+ * @type struct<Region>
+ * 
+ * @param region 72
+ * @type struct<Region>
+ * 
+ * @param region 73
+ * @type struct<Region>
+ * 
+ * @param region 74
+ * @type struct<Region>
+ * 
+ * @param region 75
+ * @type struct<Region>
+ * 
+ * @param region 76
+ * @type struct<Region>
+ * 
+ * @param region 77
+ * @type struct<Region>
+ * 
+ * @param region 78
+ * @type struct<Region>
+ * 
+ * @param region 79
+ * @type struct<Region>
+ * 
+ * @param region 80
+ * @type struct<Region>
+ * 
+ * @param region 81
+ * @type struct<Region>
+ * 
+ * @param region 82
+ * @type struct<Region>
+ * 
+ * @param region 83
+ * @type struct<Region>
+ * 
+ * @param region 84
+ * @type struct<Region>
+ * 
+ * @param region 85
+ * @type struct<Region>
+ * 
+ * @param region 86
+ * @type struct<Region>
+ * 
+ * @param region 87
+ * @type struct<Region>
+ * 
+ * @param region 88
+ * @type struct<Region>
+ * 
+ * @param region 89
+ * @type struct<Region>
+ * 
+ * @param region 90
+ * @type struct<Region>
+ * 
+ * @param region 91
+ * @type struct<Region>
+ * 
+ * @param region 92
+ * @type struct<Region>
+ * 
+ * @param region 93
+ * @type struct<Region>
+ * 
+ * @param region 94
+ * @type struct<Region>
+ * 
+ * @param region 95
+ * @type struct<Region>
+ * 
+ * @param region 96
+ * @type struct<Region>
+ * 
+ * @param region 97
+ * @type struct<Region>
+ * 
+ * @param region 98
+ * @type struct<Region>
+ * 
+ * @param region 99
+ * @type struct<Region>
+ * 
+ * @param region 100
+ * @type struct<Region>
+ * 
+ * @param region 101
+ * @type struct<Region>
+ * 
+ * @param region 102
+ * @type struct<Region>
+ * 
+ * @param region 103
+ * @type struct<Region>
+ * 
+ * @param region 104
+ * @type struct<Region>
+ * 
+ * @param region 105
+ * @type struct<Region>
+ * 
+ * @param region 106
+ * @type struct<Region>
+ * 
+ * @param region 107
+ * @type struct<Region>
+ * 
+ * @param region 108
+ * @type struct<Region>
+ * 
+ * @param region 109
+ * @type struct<Region>
+ * 
+ * @param region 110
+ * @type struct<Region>
+ * 
+ * @param region 111
+ * @type struct<Region>
+ * 
+ * @param region 112
+ * @type struct<Region>
+ * 
+ * @param region 113
+ * @type struct<Region>
+ * 
+ * @param region 114
+ * @type struct<Region>
+ * 
+ * @param region 115
+ * @type struct<Region>
+ * 
+ * @param region 116
+ * @type struct<Region>
+ * 
+ * @param region 117
+ * @type struct<Region>
+ * 
+ * @param region 118
+ * @type struct<Region>
+ * 
+ * @param region 119
+ * @type struct<Region>
+ * 
+ * @param region 120
+ * @type struct<Region>
+ * 
+ * @param region 121
+ * @type struct<Region>
+ * 
+ * @param region 122
+ * @type struct<Region>
+ * 
+ * @param region 123
+ * @type struct<Region>
+ * 
+ * @param region 124
+ * @type struct<Region>
+ * 
+ * @param region 125
+ * @type struct<Region>
+ * 
+ * @param region 126
+ * @type struct<Region>
+ * 
+ * @param region 127
+ * @type struct<Region>
+ * 
+ * @param region 128
+ * @type struct<Region>
+ * 
+ * @param region 129
+ * @type struct<Region>
+ * 
+ * @param region 130
+ * @type struct<Region>
+ * 
+ * @param region 131
+ * @type struct<Region>
+ * 
+ * @param region 132
+ * @type struct<Region>
+ * 
+ * @param region 133
+ * @type struct<Region>
+ * 
+ * @param region 134
+ * @type struct<Region>
+ * 
+ * @param region 135
+ * @type struct<Region>
+ * 
+ * @param region 136
+ * @type struct<Region>
+ * 
+ * @param region 137
+ * @type struct<Region>
+ * 
+ * @param region 138
+ * @type struct<Region>
+ * 
+ * @param region 139
+ * @type struct<Region>
+ * 
+ * @param region 140
+ * @type struct<Region>
+ * 
+ * @param region 141
+ * @type struct<Region>
+ * 
+ * @param region 142
+ * @type struct<Region>
+ * 
+ * @param region 143
+ * @type struct<Region>
+ * 
+ * @param region 144
+ * @type struct<Region>
+ * 
+ * @param region 145
+ * @type struct<Region>
+ * 
+ * @param region 146
+ * @type struct<Region>
+ * 
+ * @param region 147
+ * @type struct<Region>
+ * 
+ * @param region 148
+ * @type struct<Region>
+ * 
+ * @param region 149
+ * @type struct<Region>
+ * 
+ * @param region 150
+ * @type struct<Region>
+ * 
+ * @param region 151
+ * @type struct<Region>
+ * 
+ * @param region 152
+ * @type struct<Region>
+ * 
+ * @param region 153
+ * @type struct<Region>
+ * 
+ * @param region 154
+ * @type struct<Region>
+ * 
+ * @param region 155
+ * @type struct<Region>
+ * 
+ * @param region 156
+ * @type struct<Region>
+ * 
+ * @param region 157
+ * @type struct<Region>
+ * 
+ * @param region 158
+ * @type struct<Region>
+ * 
+ * @param region 159
+ * @type struct<Region>
+ * 
+ * @param region 160
+ * @type struct<Region>
+ * 
+ * @param region 161
+ * @type struct<Region>
+ * 
+ * @param region 162
+ * @type struct<Region>
+ * 
+ * @param region 163
+ * @type struct<Region>
+ * 
+ * @param region 164
+ * @type struct<Region>
+ * 
+ * @param region 165
+ * @type struct<Region>
+ * 
+ * @param region 166
+ * @type struct<Region>
+ * 
+ * @param region 167
+ * @type struct<Region>
+ * 
+ * @param region 168
+ * @type struct<Region>
+ * 
+ * @param region 169
+ * @type struct<Region>
+ * 
+ * @param region 170
+ * @type struct<Region>
+ * 
+ * @param region 171
+ * @type struct<Region>
+ * 
+ * @param region 172
+ * @type struct<Region>
+ * 
+ * @param region 173
+ * @type struct<Region>
+ * 
+ * @param region 174
+ * @type struct<Region>
+ * 
+ * @param region 175
+ * @type struct<Region>
+ * 
+ * @param region 176
+ * @type struct<Region>
+ * 
+ * @param region 177
+ * @type struct<Region>
+ * 
+ * @param region 178
+ * @type struct<Region>
+ * 
+ * @param region 179
+ * @type struct<Region>
+ * 
+ * @param region 180
+ * @type struct<Region>
+ * 
+ * @param region 181
+ * @type struct<Region>
+ * 
+ * @param region 182
+ * @type struct<Region>
+ * 
+ * @param region 183
+ * @type struct<Region>
+ * 
+ * @param region 184
+ * @type struct<Region>
+ * 
+ * @param region 185
+ * @type struct<Region>
+ * 
+ * @param region 186
+ * @type struct<Region>
+ * 
+ * @param region 187
+ * @type struct<Region>
+ * 
+ * @param region 188
+ * @type struct<Region>
+ * 
+ * @param region 189
+ * @type struct<Region>
+ * 
+ * @param region 190
+ * @type struct<Region>
+ * 
+ * @param region 191
+ * @type struct<Region>
+ * 
+ * @param region 192
+ * @type struct<Region>
+ * 
+ * @param region 193
+ * @type struct<Region>
+ * 
+ * @param region 194
+ * @type struct<Region>
+ * 
+ * @param region 195
+ * @type struct<Region>
+ * 
+ * @param region 196
+ * @type struct<Region>
+ * 
+ * @param region 197
+ * @type struct<Region>
+ * 
+ * @param region 198
+ * @type struct<Region>
+ * 
+ * @param region 199
+ * @type struct<Region>
+ * 
+ * @param region 200
+ * @type struct<Region>
+ * 
+ * @param region 201
+ * @type struct<Region>
+ * 
+ * @param region 202
+ * @type struct<Region>
+ * 
+ * @param region 203
+ * @type struct<Region>
+ * 
+ * @param region 204
+ * @type struct<Region>
+ * 
+ * @param region 205
+ * @type struct<Region>
+ * 
+ * @param region 206
+ * @type struct<Region>
+ * 
+ * @param region 207
+ * @type struct<Region>
+ * 
+ * @param region 208
+ * @type struct<Region>
+ * 
+ * @param region 209
+ * @type struct<Region>
+ * 
+ * @param region 210
+ * @type struct<Region>
+ * 
+ * @param region 211
+ * @type struct<Region>
+ * 
+ * @param region 212
+ * @type struct<Region>
+ * 
+ * @param region 213
+ * @type struct<Region>
+ * 
+ * @param region 214
+ * @type struct<Region>
+ * 
+ * @param region 215
+ * @type struct<Region>
+ * 
+ * @param region 216
+ * @type struct<Region>
+ * 
+ * @param region 217
+ * @type struct<Region>
+ * 
+ * @param region 218
+ * @type struct<Region>
+ * 
+ * @param region 219
+ * @type struct<Region>
+ * 
+ * @param region 220
+ * @type struct<Region>
+ * 
+ * @param region 221
+ * @type struct<Region>
+ * 
+ * @param region 222
+ * @type struct<Region>
+ * 
+ * @param region 223
+ * @type struct<Region>
+ * 
+ * @param region 224
+ * @type struct<Region>
+ * 
+ * @param region 225
+ * @type struct<Region>
+ * 
+ * @param region 226
+ * @type struct<Region>
+ * 
+ * @param region 227
+ * @type struct<Region>
+ * 
+ * @param region 228
+ * @type struct<Region>
+ * 
+ * @param region 229
+ * @type struct<Region>
+ * 
+ * @param region 230
+ * @type struct<Region>
+ * 
+ * @param region 231
+ * @type struct<Region>
+ * 
+ * @param region 232
+ * @type struct<Region>
+ * 
+ * @param region 233
+ * @type struct<Region>
+ * 
+ * @param region 234
+ * @type struct<Region>
+ * 
+ * @param region 235
+ * @type struct<Region>
+ * 
+ * @param region 236
+ * @type struct<Region>
+ * 
+ * @param region 237
+ * @type struct<Region>
+ * 
+ * @param region 238
+ * @type struct<Region>
+ * 
+ * @param region 239
+ * @type struct<Region>
+ * 
+ * @param region 240
+ * @type struct<Region>
+ * 
+ * @param region 241
+ * @type struct<Region>
+ * 
+ * @param region 242
+ * @type struct<Region>
+ * 
+ * @param region 243
+ * @type struct<Region>
+ * 
+ * @param region 244
+ * @type struct<Region>
+ * 
+ * @param region 245
+ * @type struct<Region>
+ * 
+ * @param region 246
+ * @type struct<Region>
+ * 
+ * @param region 247
+ * @type struct<Region>
+ * 
+ * @param region 248
+ * @type struct<Region>
+ * 
+ * @param region 249
+ * @type struct<Region>
+ * 
+ * @param region 250
+ * @type struct<Region>
+ * 
+ * @param region 251
+ * @type struct<Region>
+ * 
+ * @param region 252
+ * @type struct<Region>
+ * 
+ * @param region 253
+ * @type struct<Region>
+ * 
+ * @param region 254
+ * @type struct<Region>
+ * 
+ * @param region 255
+ * @type struct<Region>
+ * 
+ * 
+ * 
+ * @help
+ * ----------------------------------------------------------------------------
+ * Introduction
+ * ----------------------------------------------------------------------------
+ *
+ * Are you bored of creating a ton of events to start bgm's in certain areas?
+ * Well, this is the plugin for you! Just by changing a few of the parameter's values,
+ * the designated regions will be attributed a certain BGM! Amazing!
+ * 
+ * For any region of your choosing, simply set the BGM name to one of the names
+ * of the BGMs (case sensitive!), as well as the volume, pitch and pan to a
+ * value of your liking.
+ * If you do not select Instant BGM, you will be able to define the fadein and
+ * fadeout time of the previous and current BGM!
+ * 
+ * For more information, dm DaedraTalos in the RPG Maker Forums, or DaedraKyne
+ * in one of the many RMMV discord servers. Have fun! :D
+ * 
+ * ----------------------------------------------------------------------------
+ * Script calls
+ * ----------------------------------------------------------------------------
+ *
+ * 
+ * ----------------------------------------------------------------------------
+ * Changelog
+ * ----------------------------------------------------------------------------
+ * 
+ * 
+//----------------------------------------------------------------------------
+*/
+/*~struct~Region:
+ * @param Region Name
+ * @type text
+ * 
+ * @param Region BGM Name
+ * @type text
+ * 
+ * @param BGM volume
+ * @type number
+ * @min 0
+ * @max 100
+ * 
+ * @param BGM pitch
+ * @type number
+ * @min 0
+ * @max 100
+ * 
+ * @param BGM pan
+ * @type number
+ * 
+ * @param Instant BGM
+ * @type boolean
+ * @desc If false and a BGM is already playing, it will fade out before starting this BGM. Otherwise, it will cut the other BGM and instantly start this one.
+ * 
+ * @param fadeout time
+ * @type number
+ * @desc If Instant BGM was set to false, the time in seconds that it takes for the previous region's BGM to fadeout.
+ * 
+ * @param fadein time
+ * @type number
+ * @desc If Instant BGM was set to false, the time in seconds that it takes for the current region's BGM to fadein. 
+ */
+
+/*
+TO USE: $gamePlayer.regionId()
+AudioManager.playBgm({name: "Theme6", volume: 90, pitch: 100, pan: 0});
+
+*/
+
+(function () {
+
+    var parameters = PluginManager.parameters("Region BGM"); 
+
+    var playerRegionId = 0;
+    var currentPlayerRegionId = 0;
+    var waitTime = 0;
+    var currentWaitTime = 0;
+
+    var _Scene_Map_Update = Scene_Map.prototype.update;
+    Scene_Map.prototype.update = function() {
+        currentPlayerRegionId = $gamePlayer.regionId();
+        if (currentPlayerRegionId != playerRegionId && currentPlayerRegionId != 0) {
+            playerRegionId = currentPlayerRegionId;
+            var region = JSON.parse(parameters["region " + String(currentPlayerRegionId)]);
+            console.log(region["fadeout time"], parseInt(region["fadeout time"]), region["Instant BGM"]);
+            if (region["Instant BGM"] == "true") {
+                AudioManager.playBgm({name: region["Region BGM Name"], volume: region["BGM volume"], pitch: region["BGM pitch"], pan: region["BGM pan"]});
+            }
+            else {
+                console.log("fading");
+                AudioManager.fadeOutBgm(parseInt(region["fadeout time"]));
+                waitTime = parseInt(region["fadeout time"]) * 60;
+                currentWaitTime += 1;
+                
+            }
+        } else {
+            if (currentWaitTime < waitTime) {
+                currentWaitTime += 1;
+                console.log("waiting", currentWaitTime);
+            }
+            else if (currentWaitTime == waitTime && waitTime != 0) {
+                console.log("Fading in bgm");
+                var region = JSON.parse(parameters["region " + String(currentPlayerRegionId)]);
+                AudioManager.playBgm({name: region["Region BGM Name"], volume: region["BGM volume"], pitch: region["BGM pitch"], pan: region["BGM pan"]});
+                console.log(parseInt(region["fadein time"]));
+                AudioManager.fadeInBgm(parseInt(region["fadein time"]));
+                waitTime = 0;
+                currentWaitTime = 0;
+            }
+        }
+        _Scene_Map_Update.call(this);
+    };
+
+})();
